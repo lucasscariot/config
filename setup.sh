d0ebe1c331ad7ac9a87924a6eaf164ae5d84235c
Lucas <lucas@scariot.fr>
2018-01-12 14:34:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,71 @@
-if ! command -v brew $> /dev/null ; then
+if ! command -v brew &> /dev/null ; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-  echo "Brew already installed."
+  echo "Already installed       Brew"
 fi
 
-function brewInstall() {
-  if command -v $1 $> /dev/null ; then
-    echo "$1 Already installed."
+function notInstalled() {
+  if command -v $1 &> /dev/null ; then
+    echo "Already installed       $1"
     return 1
-  fi
-  brew install $1
-  if [ $? -ne 0 ]; then
-    echo "$1 Installation failed."
-    exit
+  else
+    echo "Installing              $1"
+    return 0
   fi
 }
 
-brew update
+function brewInstall() {
+  [[ $2 ]] && program="$2" || program="$1"
+
+  if notInstalled $program ; then
+    brew install $1 &> /dev/null
+    if [ $? -ne 0 ]; then
+      echo "    -> Installation failed."
+      exit
+    else
+      echo "    -> Installation success!"
+    fi
+  fi
+}
+
+function npmInstall() {
+  [[ $2 ]] && program="$2" || program="$1"
+
+  if notInstalled $program ; then
+    npm install $1 --global &> /dev/null
+    if [ $? -ne 0 ]; then
+      echo "    -> Installation failed."
+      exit
+    else
+      echo "    -> Installation success!"
+    fi
+  fi
+}
+
+brew update &> /dev/null
 
 brewInstall wget
 brewInstall node
 brewInstall vim
 brewInstall zsh
+brewInstall postgresql psql
+brewInstall redis redis-cli
+brewInstall gnupg, gpg
 
-wget https://download.docker.com/mac/stable/Docker.dmg
-hdutil mount Docker.dmg
-cp -R "/Volumes/Docker/docker.app" /Application
-hdiutil detach /Volumes/Docker
-open /Applications/docker
+if notInstalled docker ; then
+  wget https://download.docker.com/mac/stable/Docker.dmg
+  hdiutil mount Docker.dmg
+  cp -R "/Volumes/Docker/Docker.app" /Applications
+  hdiutil detach /Volumes/Docker
+  open /Applications/Docker.app
+  echo "Configure your docker.. press enter when it's done"
+  read
+  if ! notInstalled docker-compose ; then
+    docker-compose up -d
+  fi
+  rm Docker.dmg
+  # pg_restore -O -d forest ~/Downloads/forest.dump -h localhost -p 5435 -U forest
+fi
 
-npm install --global release
-npm install --global nodemon
+npmInstall nodemon
+npmInstall release

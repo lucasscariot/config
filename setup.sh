46febdb6fb44d404d58242c2440e0ca9b422b949
Lucas <lucas@scariot.fr>
2018-01-12 15:43:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,3 @@
-if ! command -v brew &> /dev/null ; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo "Already installed       Brew"
-fi
-
 function notInstalled() {
   if command -v $1 &> /dev/null ; then
     echo "Already installed       $1"
@@ -14,18 +8,22 @@ function notInstalled() {
   fi
 }
 
+function installResult() {
+  if [ $? -ne 0 ]; then
+    echo "    -> Installation failed."
+    exit
+  else
+    echo "    -> Installation success!"
+    source ~/.bash_profile
+  fi
+}
+
 function brewInstall() {
-  [[ $2 ]] && program="$2" || program="$1"
+  [[ $2 ]] && program=$2 || program=$1
 
   if notInstalled $program ; then
     brew install $1 &> /dev/null
-    if [ $? -ne 0 ]; then
-      echo "    -> Installation failed."
-      exit
-    else
-      echo "    -> Installation success!"
-      source ~/.bash_profile
-    fi
+    installResult
   fi
 }
 
@@ -34,13 +32,7 @@ function npmInstall() {
 
   if notInstalled $program ; then
     npm install $1 --global &> /dev/null
-    if [ $? -ne 0 ]; then
-      echo "    -> Installation failed."
-      exit
-    else
-      echo "    -> Installation success!"
-      source ~/.bash_profile
-    fi
+    installResult
   fi
 }
 
@@ -49,18 +41,15 @@ function gemInstall() {
 
   if notInstalled $program ; then
     gem install $1 &> /dev/null
-    if [ $? -ne 0 ]; then
-      echo "    -> Installation failed."
-      exit
-    else
-      echo "    -> Installation success!"
-      source ~/.bash_profile
-    fi
+    installResult
   fi
 }
 
-brew update &> /dev/null
+if notInstalled brew ; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
+brew update &> /dev/null
 brewInstall wget
 brewInstall node
 brewInstall vim
@@ -71,7 +60,7 @@ brewInstall gnupg, gpg
 brewInstall rbenv
 brewInstall openssl
 
-#  DOCKER
+#  Install Docker
 if notInstalled docker ; then
   wget https://download.docker.com/mac/stable/Docker.dmg
   hdiutil mount Docker.dmg
